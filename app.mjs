aa7db0803f155ee32a5e466af83e8eee349b1f01
Apisit <fu.apisit@gmail.com>
2025-04-13 21:57:33 +0700

    feat: setup project

--- app.mjs
+++ app.mjs
@@ -1,14 +1,14 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
-const port = 4000;
-
-app.use(express.json());
+app.use(express.json()); //express อ่านค่าจาก body ได้
+const PORT = 4000;
 
 app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
+  return res.json("✅ Server API is working");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
